1732e02d209daca8851bf4a2982869c9f6bb708b
HAOTIAN LIU <shenglongzhihou1@gmail.com>
2023-10-31 16:54:26 +1000

    prac06

---
+++ prac_06/programming_language.py
@@ -0,0 +1,24 @@
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+
+def run_tests():
+
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    if __name__ == "__main__":
+        run_tests()
