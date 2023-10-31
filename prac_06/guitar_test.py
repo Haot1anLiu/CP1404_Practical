0750cc6a6ed92ee19743e2ad18b87facfe3ef64f
HAOTIAN LIU <shenglongzhihou1@gmail.com>
2023-10-31 21:08:03 +1000

    prac06

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -10,8 +10,8 @@ def run_tests():
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2013, 1512.9)
 
-    print(f"{guitar.name} get_age() - Expected {100}. Got {guitar.get_age()}")
-    print(f"{guitar.name} get_age() - Expected {9}. Got {other.get_age()}")
+    print(f"{guitar.name} get_age() - Expected {101}. Got {guitar.get_age()}")
+    print(f"{guitar.name} get_age() - Expected {10}. Got {other.get_age()}")
     print()
     print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
     print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
